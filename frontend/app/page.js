85a64e799d2facc1571dd7cb7e6152b3a380f78d
soumojit banerjee <soumojitbanerjee08@gmail.com>
2026-01-28 19:06:46 +0530

    setup frontend

---
+++ frontend/app/page.js
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { CircleFadingArrowUpIcon } from "lucide-react"
+
+export default function Home() {
+  return (
+    <div>
+        <Button variant="outline" size="icon">
+      <CircleFadingArrowUpIcon />
+    </Button>
+    </div>
+  );
+}
